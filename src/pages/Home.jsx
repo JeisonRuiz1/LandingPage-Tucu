c14e41a197b44b5821e6a12c2c94fd2792ce2604
JeisonRuiz0 <JeisonRuiz1910@gmail.com>
2022-06-08 15:19:51 -0500

    [update v3xd]

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,11 @@ import InformationComponent from "../components/InformationComponent";
 import Benefits from "../containers/Benefits";
 import HelpForClients from "../containers/HelpForClients";
 import AboutUs from "../containers/AboutUs";
-import Media from "../containers/Media";
 import AmountServices from "../containers/AmountServices";
-import Investors from "../containers/Investors";
 import Footer from "../components/Footer";
-import eligenIMG from '../icons/POR QUE NOS ELIGEN.PNG';
-import repartidorIMG from '../icons/QUIERO SER REPARTIDOR.PNG';
-import tarifasIMG from '../icons/CONOCE NUESTRAS TARIFAS.png';
+import eligenIMG from '../icons/eligenIMG.PNG';
+import repartidorIMG from '../icons/repartidorIMG.PNG';
+import tarifasIMG from '../icons/tarifasIMG.png';
 
 const Home = () => {
     return (
@@ -25,14 +23,12 @@ const Home = () => {
             />
             <Benefits />
             <HelpForClients tittle={"Clientes que podemos ayudar"} textButton={"Contáctanos"} />
-            {/* <Media /> */}
             <InformationComponent
                 tittle={"Quiero ser repartidor TUCU"}
                 content={"Trabaja con nosotros y obtén TODOS los beneficios de un trabajo formal LEGAL. Puedes generar ingresos extra con un contrato de tiempo incompleto, pero si buscas trabajo de tiempo completo también eres bienvenid@."}
                 urlImage={repartidorIMG}
                 textButton={"Me interesa"} />
             <AmountServices title={"Cifras"} />
-            {   /* <Investors title={"Nuestros inversionistas"} /> */}
             <InformationComponent
                 tittle={"Conoce nuestras tarifas"}
                 content={"Para TUCU es importante conocer el número aproximado de entregas que realiza tu marca en días y horas de alta y baja demanda, con el fin de tener el personal necesario disponible para hacer determinadas entregas. Al realizar una alianza comercial con TUCU, tu marca contará con cobertura del volumen que tu marca necesita. ¡Contáctenos! y conoce los precios de tu ciudad."}
